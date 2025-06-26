4fd0b542b7657e0ace9063773cea424727768a02
cabralcodes <matheusaugustodev.web@gmail.com>
2025-06-26 15:43:52 -0300

    terminei a parte de crair um iframe de musica, so falta a parte de armazenar ele e o projeto ta pronto

---
+++ app/pages/music.js
@@ -0,0 +1,72 @@
+let InputNome;
+let InputMusica;
+let BtnAdicionar;
+let DivBtnMusicas = document.getElementById("musics");
+let Player = document.getElementById("video");
+let Musicas = [];
+
+function ExtrairID(link){
+    let id="";
+         if (link.includes("youtube.com/watch?v=")) {
+    const url = new URL(link);
+    id = url.searchParams.get("v"); 
+  }
+
+  // Caso 2: Link encurtado do youtu.be
+  else if (link.includes("youtu.be/")) {
+    const partes = link.split("/");
+    id = partes[partes.length - 1].split("?")[0];  
+  }
+
+  
+  return id;
+}
+function PegarMusica(){
+    InputNome = document.getElementById("name__music");
+    InputMusica = document.getElementById("url__music");
+
+    BtnAdicionar = document.getElementById("adicionar");
+     BtnAdicionar.addEventListener("click",(e)=>{
+        e.preventDefault();
+       let Musica = {
+            Nome: InputNome.value,
+            Link: InputMusica.value,
+        }
+        if (Musica.Nome && Musica.Link) {
+        Musicas.push(Musica);
+
+        let BtnMusica = document.createElement("button");
+        BtnMusica.className = "name__musics";
+        BtnMusica.textContent = InputNome.value;
+        DivBtnMusicas.append(BtnMusica);
+
+        BtnMusica.addEventListener("click", ()=>{
+            let BtnLimpar = document.createElement("button");
+            BtnLimpar.className = "Btn__limpar";
+            BtnLimpar.textContent = "Fechar";
+            DivBtnMusicas.append(BtnLimpar);
+
+            BtnLimpar.addEventListener("click",()=>{
+                Player.innerHTML = "";
+                BtnLimpar.remove();
+            })
+            const idVideo = ExtrairID(Musica.Link);
+                const embedLink = `https://www.youtube.com/embed/${idVideo}`;
+
+                const iframe = document.createElement("iframe");
+                iframe.src = embedLink;
+                iframe.width = "350";
+                iframe.height = "220";
+                iframe.allowFullscreen = true;
+
+                Player.appendChild(iframe);
+        });
+    
+
+}
+});
+
+}
+
+
+PegarMusica();
